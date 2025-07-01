bc43b374f5ca7a78d2100793ed3aee4d10f802f5
Gakiri001 <gakirijulius@gmail.com>
2025-07-01 12:44:57 +0300

    resume setup

---
+++ src/data/resume.js
@@ -0,0 +1,41 @@
+const experience = [
+    {
+    station : "Wheels & Badges ltd",
+    from: "04/2025",
+    to:"current",
+    title: "Full-stack Developer - Website Development Contract",
+    role1: "Design and maintain a responsive and user-friendly interface to enhance customer experience.",
+    role2: "Build and maintain a full-stack web application for selling car spare parts using technologies such as React.js, Node.js, Express.js, and MongoDB.",
+    role3: "Integrate the M-Pesa payment gateway to enable customers to make secure mobile payments.",
+    role4: "Implement and manage user authentication and authorization for clients and sellers using JWT and secure password hashing.",
+    role5: "Deploy and host the website on a cloud platform, ensuring performance, uptime, and scalability.",
+    },
+    {
+    station: "Beth Epharaim",
+    from: "10/2024",
+    to: "11/2024",
+    title: "Frontend Web Developer - Website Development Contract",
+    link: "https://beth-portfolio.vercel.app/",
+    role1: "Translated wireframes into functional code to bring visual concepts to life.",
+    role2: "Generated a user-friendly interface by utilising React JS.",
+    role3: "Ensured seamless visuals across devices through responsive web design.",
+    role4: "Optimised web pages for maximum speed and scalability.",
+  },
+  {
+    station: "Teach2Give Company",
+    from: "05/2024",
+    to: "08/2024",
+    title: "FullStack Web Developer Intern",
+    role1: "Developed innovative web applications to improve user experience and optimized website performance and responsiveness.",
+    role2: "Developed and maintained server-side logic using Node.js, Express.js, and Django.",
+    role3: "Worked with databases (MongoDB, PostgreSQL, or MySQL) to manage and retrieve data efficiently.",
+    role4: "Implemented authentication and authorization systems using JWT, OAuth, and Firebase Authentication.",
+    role5: "Achieved and consumed RESTful APIs to connect front-end and back-end services. Integrated third-party APIs for features like payment gateways, geolocation, and external data fetching.",
+    role6: "Used Git and GitHub for version control, code collaboration, and tracking changes.",
+    role7: "Identified and resolved bugs to improve application performance. Achieved and ran unit and integration tests to ensure application reliability.",
+    role8: "Deployed web applications using platforms like Vercel and Netlify.",
+    role9: "Collaborated with team members, documented project progress, issues, and solutions, and presented findings, demos, or project updates to the supervisor."
+  }
+]
+
+export default experience;
--- src/pages/Resume/Resume.jsx
+++ src/pages/Resume/Resume.jsx
@@ -1,7 +1,60 @@
 import React from "react";
+import { FaBlackTie } from "react-icons/fa";
+import "./Resume.css";
+import { FaBriefcase } from 'react-icons/fa';
+import experience from "../../data/resume";
+import education from "../../data/education";
 
 function Resume() {
-  return <div className="bg-red-500">Resume</div>;
+  return (
+    <div className="resume">
+      <div className="resumeHeader">
+        <h2>Resume</h2>
+        <FaBlackTie className="resumeicon" />
+      </div>
+      <section>
+        <div>
+          <h3>Experience</h3>
+              <div>
+                {experience.map((index, i) => (
+                  <div key={i}>
+                    <div><FaBriefcase/></div>
+                    <h3>{index.station}</h3>
+                    <h4>Title: {index.title}</h4>
+                    <p>{index.from} - {index.to}</p>
+                    <ul>
+                      <li>{index.role1}</li>
+                      <li>{index.role2}</li>
+                      <li>{index.role3}</li>
+                      <li>{index.role4}</li>
+                      <li>{index.role5}</li>
+                      <li>{index.role6}</li>
+                      <li>{index.role7}</li>
+                      <li>{index.role8}</li>
+                      <li>{index.role9}</li>
+                    </ul>
+                  </div>
+                ))}                
+              </div>
+        </div>
+        <div>
+          <h3>Education</h3>
+          <div>
+            {education.map((eduIndex, i) =>(
+              <div key={i}>
+                <div><FaBriefcase/></div>
+                <h3>{eduIndex.school}</h3>
+                <p>{eduIndex.course}</p>
+                <p>{eduIndex.from} - {eduIndex.to}</p>
+              </div>
+            ))
+
+            }
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 }
 
 export default Resume;
