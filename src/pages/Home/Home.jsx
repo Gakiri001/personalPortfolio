9601d416b21d00cd5b1237090e9dab183affe445
Gakiri001 <gakirijulius@gmail.com>
2025-09-15 10:54:50 +0300

    adding motion and animation to the home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -4,20 +4,72 @@ import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaYoutube } from "react-icons/fa";
 import { FaFacebook } from "react-icons/fa";
 import "./Home.css";
-import { Button } from "@/components/ui/button";
+import { motion } from "framer-motion";
+import Typewriter from "typewriter-effect";
 
 function Home() {
   return (
     <div className="Home">
-      <div className="me">
+      {/* <div className="me">
         <h2>Hello, I'm Julius.</h2>
         <h2>Full-stack Developer</h2>
-      </div>
-      <div className="div-buttons">
+      </div> */}
+      <motion.div
+        className="me"
+        initial={{opacity:0, y: -50}}
+        animate={{opacity:1, y:0}}
+        transition={{duration:1}}
+      >
+        <h2>
+          <Typewriter
+          options={{
+            autoStart: true,
+            loop:false,
+            delay: 70,
+            deleteSpeed: 50,
+            cursor: ""
+          }}
+          onInit={(Typewriter) => {
+            Typewriter
+            .typeString("Hello, I'm Julius")
+            .start();
+          }}
+          />
+        </h2>
+        <h2>
+          <Typewriter
+          options={{
+            strings:["Full-stack Developer", "Data & AI Enthuasiast", "Junior Accountant"],
+            autoStart: true,
+            loop:true,
+            delay: 70,
+            deleteSpeed: 50,
+          }}
+          />
+          </h2>
+      </motion.div>
+      {/* <div className="div-buttons">
         <button className="buttonMore">Contact Me</button>
         <button className="buttonDownload">Download Resume</button>
-      </div>
-      <div className="divLinks">
+      </div> */}
+
+      {/*Buttons animate from right  */}
+      <motion.div
+      className="div-buttons"
+      initial={{x:200, opacity:0}}
+      animate={{x:0, opacity:1}}
+      transition={{duration:1, delay:1}}
+      >
+        <button className="buttonMore">Contact Me</button>
+        <button className="buttonDownload">Download Resume</button>
+      </motion.div>
+
+      <motion.div 
+      className="divLinks"
+      initial={{x:-200, opacity:0}}
+      animate={{x:0, opacity:1}}
+      transition={{duration:1, delay:1.5}}
+      >
         <a
           href="https://www.linkedin.com/in/julius-irungu-87ab54362/"
           target="_blank"
@@ -33,7 +85,7 @@ function Home() {
         <a href="https://www.facebook.com/042software" target="_blank">
           <FaFacebook className="reactIcons text-blue-900  hover:text-blue-400" />
         </a>
-      </div>
+      </motion.div>
     </div>
   );
 }
